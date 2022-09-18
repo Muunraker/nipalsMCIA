1307cf3067a6c76ee575460f3110f5e0ea4bb60a
muunraker <24959832+Muunraker@users.noreply.github.com>
2022-09-18 10:36:09 -0500

    modified files and package infrastructure to meet bioconductor requirements (incomplete)

---
+++ R/data_blocks.R
@@ -0,0 +1,10 @@
+#' @name data_blocks
+#' @title NCI-60 Multi-Omics Data
+#' @description A  dataset of mesurements of 12,895 mRNA, 537 miRNA, 
+#' and 7,016 protein variables (columns) on 21 cancer cell lines (rows) from
+#' the NCI-60 cancer cell line database.  
+#' 
+#' Data originally from the supplementary materials of Meng et. al, 2016.
+#' @references \url{https://github.com/aedin/NCI60Example}
+#' @keywords data multi-omics
+NULL
