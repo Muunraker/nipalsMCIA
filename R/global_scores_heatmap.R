2c4a729dc7390f3c8a72b3c4368f5d837601ce54
Joaquin <j2reyna@ucsd.edu>
2022-10-07 16:02:39 -0700

    Throw an error when no metadata is present + coloring

--- R/global_scores_heatmap.R
+++ R/global_scores_heatmap.R
@@ -7,39 +7,62 @@
 #'   used for coloring
 #' @return ComplexHeatmap object
 #' @export
-global_scores_heatmap <- function(mcia_result, coloring=1){
+global_scores_heatmap <- function(mcia_result, coloring=NULL){
     
     
-    # extract the column names (if not provided via coloring)
-    if (typeof(coloring) == "double"){
-        color_col = names(mcia_result$metadata)[coloring]
-    }
-    
-    # extract color type and create a color palette list
-    cat_values <- mcia_result$metadata[,coloring]
-    cat_colors <- get_metadata_colors(mcia_result, coloring = coloring)
-    cat_colors_list <- list(ColorType = cat_colors)
-    
-    # add the colors list to a HeatmapAnnotation obj
-    row_ha = ComplexHeatmap::HeatmapAnnotation(which = "row", 
-                               ColorType = cat_values,
-                               col = cat_colors_list,
-                               annotation_label = color_col,
-                               show_annotation_name = FALSE)
+    # extract the global scores
+    global_scores = mcia_result$global_scores
     
     # extract and re-label global scores
-    global_scores = mcia_result$global_scores
     colnames(global_scores) = paste0('F', seq(1, ncol(global_scores)))
     
-    p = ComplexHeatmap::Heatmap(global_scores, 
-                                name = "GS Score", 
-                                column_title = "Factors",
-                                row_title = "Samples",
-                                row_names_gp = grid::gpar(fontsize = 7),
-                                show_column_names = T,
-                                show_row_names = T,
-                                row_names_side = "right",
-                                right_annotation = row_ha
-    )    
+    if (! is.null(coloring)){
+        
+        # check if metadata is within the MCIA results
+        if (! "metadata" %in% names(mcia_results)){
+            msg = paste("Cannot produce a sample coloring without metadata.",
+                        "Please re-run global_scores_heatmap after setting",
+                        "this information.")
+            stop(msg)
+        }
+    
+        # extract the column names (if not provided via coloring)
+        if (typeof(coloring) == "double"){
+            coloring = names(mcia_result$metadata)[coloring]
+        }
+        
+        # extract color type and create a color palette list
+        cat_values <- mcia_result$metadata[,coloring]
+        cat_colors <- get_metadata_colors(mcia_result, coloring = coloring)
+        cat_colors_list <- list(ColorType = cat_colors)
+        
+        # add the colors list to a HeatmapAnnotation obj
+        row_ha = ComplexHeatmap::HeatmapAnnotation(which = "row", 
+                                   ColorType = cat_values,
+                                   col = cat_colors_list,
+                                   annotation_label = coloring,
+                                   show_annotation_name = FALSE)
+        
+        p = ComplexHeatmap::Heatmap(global_scores, 
+                                    name = "GS Score", 
+                                    column_title = "Factors",
+                                    row_title = "Samples",
+                                    row_names_gp = grid::gpar(fontsize = 7),
+                                    show_column_names = T,
+                                    show_row_names = T,
+                                    row_names_side = "right",
+                                    right_annotation = row_ha
+        )    
+    }else{
+        p = ComplexHeatmap::Heatmap(global_scores, 
+                                    name = "GS Score", 
+                                    column_title = "Factors",
+                                    row_title = "Samples",
+                                    row_names_gp = grid::gpar(fontsize = 7),
+                                    show_column_names = T,
+                                    show_row_names = T,
+                                    row_names_side = "right"
+        )    
+    }
     return(p)
 }
