36a02a1cab1f96f729bcf9ea6751ad0b2bbd62b3
Joaquin <j2reyna@ucsd.edu>
2022-10-07 12:33:02 -0700

    Colors now has a function argument for more dynamic selections

--- R/get_colors.R
+++ R/get_colors.R
@@ -1,17 +1,34 @@
-#' Assigning colors to different omics (color-blindness friendly)
+#' Assigning colors to different omics (default: color-blindness friendly)
 #'
 #' @description Creates a list of omics and associated colors for plotting
 #' 
-#' @param mcia_out object returned from nipals_multiblock() function
+#' @param mcia_results object returned from nipals_multiblock() function
 #' @return List of omics with assigned colors
 #' @examples
-#' colors_omics <- get_colors(mcia_out)
+#' colors_omics <- get_colors(mcia_results)
 #' @importFrom scales viridis_pal
 #' @export
-
-get_colors <- function(mcia_out) {
-  omic_list <- names(mcia_out$block_loadings)
-  colors_omics <- scales::viridis_pal(option = "D")(length(omic_list))
+get_colors <- function(mcia_results, color_func=scales::viridis_pal) {
+  omic_list <- names(mcia_results$block_loadings)
+  colors_omics <- color_func(option = "D")(length(omic_list))
   names(colors_omics) <- omic_list
   return(colors_omics)
+}
+
+#' Assigning colors to different omics (default: color-blindness friendly)
+#'
+#' @description Creates a list of omics and associated colors for plotting
+#' 
+#' @param mcia_results object returned from nipals_multiblock() function
+#' @return List of omics with assigned colors
+#' @examples
+#' colors_omics <- get_colors(mcia_results)
+#' @importFrom scales viridis_pal
+#' @export
+get_metadata_colors <- function(mcia_results, coloring,
+                                color_func=scales::viridis_pal) {
+  meta_list <- unique(mcia_results$metadata[,coloring])
+  colors_meta <- color_func(option = "D")(length(meta_list))
+  names(colors_meta) <- meta_list
+  return(colors_meta)
 }
