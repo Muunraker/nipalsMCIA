fa806ef16a4d443e39d9ee94596b815707c27710
Joaquin <j2reyna@ucsd.edu>
2022-11-10 12:58:44 -0800

    Refactored vis_load_ord with tidy evaluation idioms

--- R/vis_load_ord.R
+++ R/vis_load_ord.R
@@ -5,6 +5,7 @@
 #' ord_loadings() functions
 #'
 #' @param gl_f_ord Ranked loading dataframe output from ord_loadings() function
+#' @omic name of the given omic dataset
 #' @param colors_omics named list of colors associated with omics,
 #' output of get_colors() function
 #' @param n_feat number of features to visualize
@@ -15,17 +16,21 @@
 #' @importFrom ggplot2 scale_color_manual
 #' @export
 
-vis_load_ord <- function(gl_f_ord, colors_omics, n_feat = 15) {
+vis_load_ord <- function(gl_f_ord, omic_name, colors_omics, n_feat = 15) {
+  loading <- rlang::quo(`loading`)
+  omic <- rlang::quo(`omic`)
   n_plot <- min(nrow(gl_f_ord), n_feat)
-  omic_subset <- names(table(droplevels(gl_f_ord[0:n_plot, ])$omic))
+  omic_subset <- names(table(droplevels(gl_f_ord[seq(0, n_plot), ])$omic))
   color_vals <- colors_omics[omic_subset]
-  p <- ggplot(data = gl_f_ord[1:n_plot, ],
-                       aes(x = factor(omic_name, level = omic_name),
-                           y = loading, color = omic)) +
-    geom_point() +
-    theme_bw() +
-    xlab("Feature") +
-    theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, size = 6))
-            + scale_color_manual(values = color_vals)
+  p <- ggplot(data = gl_f_ord[seq_len(n_plot), ],
+              ggplot2::aes(x = factor(omic_name, level = omic_name),
+                  y = !!loading, color = !!omic)) +
+       ggplot2::geom_point() +
+       ggplot2::theme_bw() +
+       ggplot2::xlab("Feature") +
+       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45,
+                                                 hjust = 1,
+                                                 size = 6)) +
+      ggplot2::scale_color_manual(values = color_vals)
   return(p)
 }
