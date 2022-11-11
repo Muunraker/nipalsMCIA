23279cf38bbe0d0accd5b82f8489c669f0188aca
Joaquin <j2reyna@ucsd.edu>
2022-11-11 06:23:06 -0800

    Completed lintr for deflate_block_gs.R

--- R/deflate_block_gs.R
+++ R/deflate_block_gs.R
@@ -1,22 +1,20 @@
-
 #' Deflation via global scores
 #'
-#' @description Removes data from a data frame in the direction of a given global 
-#' scores vector. 
-#' 
+#' @description Removes data from a data frame in the direction of a given
+#' global scores vector.
+#'
 #' @details Subtracts the component of each column in the direction of a given
 #' global scores vector to yield a `deflated' data matrix.
-#' 
-#' @param df a data frame in "sample" x "variable" format  
+#'
+#' @param df a data frame in "sample" x "variable" format
 #' @param gs a global scores vector in sample space
 #' @return the deflated data frame
-#' @examples 
+#' @examples
 #' deflated_data <- deflate_block_gs(data_frame,global_score)
-#' 
+#'
 #' @export
-deflate_block_gs <- function(df,gs){
-  normed_gs <- gs/norm(gs, type="2")
+deflate_block_gs <- function(df, gs) {
+  normed_gs <- gs / norm(gs, type = "2")
   df <- df - tcrossprod(normed_gs) %*% as.matrix(df)
   return(df)
 }
-
