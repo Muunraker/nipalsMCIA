3523e065e7a7893e02ba086437d966c1a5b5cb3d
Joaquin <j2reyna@ucsd.edu>
2022-11-11 06:07:07 -0800

    Completed lintr for global_scores_eigenvalues_plot.R

--- R/global_scores_eigenvalues_plot.R
+++ R/global_scores_eigenvalues_plot.R
@@ -1,11 +1,11 @@
 #' global_scores_eigenvalues_plot
 #'
 #' @description Function to plot eigenvalues of scores up to num_PCs
-#' 
+#'
 #' @details Plotting function for eigenvalues of scores up to num_PCs
-#' 
+#'
 #' @param mcia_results MCIA results object returned from `nipals_multiblock`
-#' 
+#'
 #' @examples
 #' data(NCI60)
 #' mcia_results <- nipals_multiblock(data_blocks, metadata = metadata_NCI60,
@@ -13,22 +13,22 @@
 #' global_scores_eigenvalues_plot(mcia_results)
 #' @return Displays the contribution plot using eigenvalues
 #' @export
-global_scores_eigenvalues_plot <- function(mcia_results){
-  
+global_scores_eigenvalues_plot <- function(mcia_results) {
+
   # Getting total variance if supplied
-  if(is.list(mcia_results$block_variances)){
-    ylabel = "Prop. Total Variance"
+  if (is.list(mcia_results$block_variances)) {
+    ylabel <- "Prop. Total Variance"
     totvar <- sum(unlist(mcia_results$block_variances))
-  } else{
-    ylabel = "Eigenvalue"
+  } else {
+    ylabel <- "Eigenvalue"
     totvar <- 1
   }
-    
-  # extract eigenvalues
-  barploteigs <- (unlist(mcia_results$eigvals))/totvar
-  names(barploteigs) <- 1:length(mcia_results$eigvals)
 
-  # generate barplot 
-  barplot(barploteigs, xlab="Factors",ylab=ylabel, cex.names = 1, 
+  # extract eigenvalues
+  barploteigs <- (unlist(mcia_results$eigvals)) / totvar
+  names(barploteigs) <- seq(1, length(mcia_results$eigvals))
+
+  # generate barplot
+  barplot(barploteigs, xlab = "Factors", ylab = ylabel, cex.names = 1,
           main = "Global Eigenvalues ")
 }
