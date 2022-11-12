29f3f4dd6d3a4b6c5a2c82b3b115e5ae9b9f275b
Joaquin <j2reyna@ucsd.edu>
2022-11-12 06:41:38 -0800

    Fixed example codes for vis_load_[ord/plot].R

--- R/vis_load_ord.R
+++ R/vis_load_ord.R
@@ -11,15 +11,25 @@
 #' @param n_feat number of features to visualize
 #' @return Plot in features for a factor by rank
 #' @examples
-#' vis_load_ord(all_pos_1_df, colors_omics = colors_omics)
+#' data(NCI60)
+#' mcia_results <- nipals_multiblock(data_blocks, metadata = metadata_NCI60,
+#' num_PCs = 10, plots = "none", tol = 1e-12)
+#' all_pos_1 <- ord_loadings(mcia_out = mcia_results, omic = "all",
+#'   absolute = FALSE, descending = TRUE, factor = 1)
+#' colors_omics <- get_colors(mcia_results)
+#' vis_load_ord(all_pos_1, colors_omics = colors_omics)
 #' @importFrom rlang quo
 #' @importFrom ggplot2 ggplot aes geom_point theme_bw xlab theme
 #' @importFrom ggplot2 scale_color_manual element_text
 #' @export
 
 vis_load_ord <- function(gl_f_ord, omic_name, colors_omics, n_feat = 15) {
+    
+  # setting parameters with tidy evaluation
   loading <- quo(`loading`)
   omic <- quo(`omic`)
+  
+  # making a plot for the given omics
   n_plot <- min(nrow(gl_f_ord), n_feat)
   omic_subset <- names(table(droplevels(gl_f_ord[seq(0, n_plot), ])$omic))
   color_vals <- colors_omics[omic_subset]
@@ -30,8 +40,8 @@ vis_load_ord <- function(gl_f_ord, omic_name, colors_omics, n_feat = 15) {
        theme_bw() +
        xlab("Feature") +
        theme(axis.text.x = element_text(angle = 45,
-                                                 hjust = 1,
-                                                 size = 6)) +
+                                        hjust = 1,
+                                        size = 6)) +
       scale_color_manual(values = color_vals)
   return(p)
 }
--- R/vis_load_plot.R
+++ R/vis_load_plot.R
@@ -9,12 +9,21 @@
 #' output of get_colors() function
 #' @return Plot of MCIA feature loadings for chosen axes
 #' @examples
-#' vis_load_plot(mcia_results, axes = c(1, 4), colors_omics)
+#' data(NCI60)
+#' mcia_results <- nipals_multiblock(data_blocks, metadata = metadata_NCI60,
+#' num_PCs = 10, plots = "none", tol = 1e-12)
+#' colors_omics <- get_colors(mcia_results)
+#' vis_load_plot(mcia_results, axes = c(1, 4), colors_omics = colors_omics)
 #' @importFrom ggplot2 ggplot aes_string geom_point theme_bw scale_color_manual
+#' @importFrom ggplot2 xlab ylab
 #' @export
 
 vis_load_plot <- function(mcia_out, axes = c(1, 2), colors_omics) {
+
+  # extracting global loadings
   gl <- mcia_out$global_loadings
+
+  # parsing data
   omic_name <- gsub("^.*_", "", rownames(gl))
   gl_f <- data.frame(gl[, axes])
   gl_f$omic <- omic_name
@@ -22,11 +31,16 @@ vis_load_plot <- function(mcia_out, axes = c(1, 2), colors_omics) {
   colnames(gl_f) <- c(paste0("Axis_", axes[1]),
                       paste0("Axis_", axes[2]),
                       "omic")
+
+  # plot data
   p <- ggplot(data = gl_f,
               aes_string(x = colnames(gl_f)[1],
-              y = colnames(gl_f)[2], color = "omic")) +
-    geom_point(alpha = 0.3) +
-    theme_bw() +
-    scale_color_manual(values = colors_omics)
+                         y = colnames(gl_f)[2],
+                         color = "omic")) +
+              xlab(paste0("Axis ", axes[1])) + 
+              ylab(paste0("Axis ", axes[2])) +
+              geom_point(alpha = 0.3) +
+              theme_bw() +
+              scale_color_manual(values = colors_omics)
   return(p)
 }
