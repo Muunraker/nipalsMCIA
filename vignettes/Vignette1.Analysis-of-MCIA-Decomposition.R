7fb58f23fe27c2d4c64e2673fa002ba49226be8c
Anna Konstorum <konstorum.anna@gmail.com>
2023-05-11 20:10:11 -0400

    updated Vignette 1 introduction

---
+++ vignettes/Vignette1.Analysis-of-MCIA-Decomposition.R
@@ -0,0 +1,13 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ----installation-github, eval = FALSE----------------------------------------
+#  # install.packages("devtools")
+#  devtools::install_github("Muunraker/nipalsMCIA", ref = "code-development",
+#                           force = TRUE, build_vignettes = TRUE)
+
+## ----installation-bioconductor, eval = FALSE----------------------------------
+#  # after acceptance
+#  # install.packages("BiocManager")
+#  BiocManager::install("nipalsMCIA")
+
