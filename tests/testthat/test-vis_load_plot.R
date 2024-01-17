c540bc69c3a6da7ecfc4b79c251af144421ebe68
Edel Aron <edel.aron@yale.edu>
2024-01-17 11:21:30 -0500

    started making more plot tests

--- tests/testthat/test-vis_load_plot.R
+++ tests/testthat/test-vis_load_plot.R
@@ -1,15 +1,20 @@
-test_that("viz_load_plot: checking figure", {
+test_that("figure generation", {
     #dev.new()
     #par(mar = c(2, 2, 2, 2))
-    expect_no_error(vis_load_plot(mcia_results, axes = c(1, 2),
+    expect_no_error(vis_load_plot(mcia_out = mcia_results, axes = c(1, 2),
                                   colors_omics = colors_omics))
     #dev.off()
 })
 
-test_that("viz_load_plot: checking incorrect axes", {
+test_that("incorrect axes", {
     #dev.new()
     #par(mar = c(2, 2, 2, 2))
-    expect_error(vis_load_plot(mcia_results, axes = c(1, 10),
+    expect_error(vis_load_plot(mcia_out = mcia_results, axes = c(1, 10),
                                colors_omics = colors_omics))
     #dev.off()
+})
+
+test_that("default axes", {
+    expect_no_error(vis_load_plot(mcia_out = mcia_results,
+                                  colors_omics = colors_omics))
 })
